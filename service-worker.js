e5faa2585db2dcd6bf818863ccf88b10894e911c
tuscatalogosvip-byte <tuscatalogosvip@gmail.com>
2025-08-03 14:31:41 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'pdf-app-v1';
+const URLS_TO_CACHE = [
+  'index.html',
+  'manifest.json',
+  'service-worker.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
